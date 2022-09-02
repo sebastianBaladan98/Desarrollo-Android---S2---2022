807c47e78961550e55125f99984a8b30b3d41130
Sebastián Baladán <sbaladan@scanntech.com>
2022-09-01 22:22:25 -0300

    Revert "Avance con Leccion 1 de vistas"
    This reverts commit d2d671fd1e25aefba14636e9d0ed4f05dac1f689.

--- VistasestilosynavegacinLeccin1/app/src/main/java/com/example/vistasestilosynavegacin_leccin1/MainActivity.kt
+++ VistasestilosynavegacinLeccin1/app/src/main/java/com/example/vistasestilosynavegacin_leccin1/MainActivity.kt
@@ -1,25 +1,15 @@
 package com.example.vistasestilosynavegacin_leccin1
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.vistasestilosynavegacin_leccin1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
         setContentView(R.layout.activity_main)
-    }
-    fun login(){
-
     }
 }
