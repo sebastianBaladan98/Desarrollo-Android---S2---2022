d2d671fd1e25aefba14636e9d0ed4f05dac1f689
Sebastián Baladán <sbaladan@scanntech.com>
2022-09-01 22:22:25 -0300

    Avance con Leccion 1 de vistas

--- VistasestilosynavegacinLeccin1/app/src/main/java/com/example/vistasestilosynavegacin_leccin1/MainActivity.kt
+++ VistasestilosynavegacinLeccin1/app/src/main/java/com/example/vistasestilosynavegacin_leccin1/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.vistasestilosynavegacin_leccin1
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.example.vistasestilosynavegacin_leccin1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var binding: ActivityMainBinding? = null
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setContentView(R.layout.activity_main)
+    }
+    fun login(){
+
     }
 }
