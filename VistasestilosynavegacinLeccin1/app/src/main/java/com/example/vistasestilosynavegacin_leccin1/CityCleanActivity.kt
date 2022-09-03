ab0dc59bf7aff30a36ae3eb30426077dd0e51cb2
Sebastián Baladán <sebastian.baladanliste@hotmail.com>
2022-09-03 16:04:27 -0300

    Falta navegar hacia la otra Activity

---
+++ VistasestilosynavegacinLeccin1/app/src/main/java/com/example/vistasestilosynavegacin_leccin1/CityCleanActivity.kt
@@ -0,0 +1,11 @@
+package com.example.vistasestilosynavegacin_leccin1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CityCleanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_city_clean)
+    }
+}
