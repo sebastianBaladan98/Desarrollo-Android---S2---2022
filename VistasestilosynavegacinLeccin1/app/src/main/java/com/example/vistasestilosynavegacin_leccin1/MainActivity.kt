6ceae24bf494b64a8196b5ff832d491f6bebf67a
Sebastián Baladán <sebastian.baladanliste@hotmail.com>
2022-09-03 16:04:47 -0300

    A

--- VistasestilosynavegacinLeccin1/app/src/main/java/com/example/vistasestilosynavegacin_leccin1/MainActivity.kt
+++ VistasestilosynavegacinLeccin1/app/src/main/java/com/example/vistasestilosynavegacin_leccin1/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.vistasestilosynavegacin_leccin1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.vistasestilosynavegacin_leccin1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,10 +13,25 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate( layoutInflater )
-        setContentView( binding.root )
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        binding.loginButton.setOnClickListener {  }
+        binding = ActivityMainBinding.inflate( layoutInflater )
+        setContentView( binding.root )
+        binding.loginButton.setOnClickListener {
+            login()
+        }
+
+    }
+
+    private fun login() {
+        val email: String = binding.emailInput.text.toString()
+        val password: String = binding.passwordInput.text.toString()
+
+        if ( email.contains("@test.com") and password.equals("Password123")) {
+            //Navegar a otra activity
+            Log.v( "TAG", "Credenciales correctas" )
+        } else {
+            Toast.makeText( this, "Credenciales incorrectas", Toast.LENGTH_SHORT ).show()
+        }
     }
 }
