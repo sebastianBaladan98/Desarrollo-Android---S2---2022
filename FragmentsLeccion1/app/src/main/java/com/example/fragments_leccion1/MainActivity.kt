c4b3aec7c09e9bb5680d5a5aed839819a8192a55
Sebastián Baladán <sebastian.baladanliste@hotmail.com>
2022-09-14 17:41:49 -0300

    Diseñados Fragments

---
+++ FragmentsLeccion1/app/src/main/java/com/example/fragments_leccion1/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.fragments_leccion1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fragments_leccion1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate( layoutInflater )
+
+        setContentView( binding.root )
+    }
+}
